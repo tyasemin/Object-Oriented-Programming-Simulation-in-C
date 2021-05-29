debf947c3dd1552259fbc21a56dbc1d875e60409
tyasemin <70871014+tyasemin@users.noreply.github.com>
2021-05-29 23:59:14 +0300

    Add files via upload

---
+++ .cpp/src/Shares.cpp
@@ -0,0 +1,52 @@
+#include "Shares.h"
+#include <iostream>
+#include <string>
+
+
+using namespace std;
+
+Shares::Shares() {  //constructor
+	this->shareName = shareName;
+	this->shareId = shareId;
+	this->sharePrice = sharePrice;
+	this->shareSysmbol = shareSysmbol;
+}
+
+string Shares::GetShareId(Shares* share) { //get
+	return share->shareId;
+}
+
+string Shares::GetShareName(Shares* share) { //get
+	return share->shareName;
+}
+
+float Shares::GetSharePrice(Shares* share) { //get
+	return share->sharePrice;
+}
+
+string Shares::GetShareSymbol(Shares* share) { //get
+	return share->shareSysmbol;
+}
+
+void Shares::SetShareId(Shares* share, string id) { //set
+	share->shareId = id;
+}
+
+void Shares::SetShareSymbol(Shares* share, string symbol) { //set
+	share->shareSysmbol = symbol;
+}
+
+void Shares::SetShareName(Shares* share, string name) { //set
+	share->shareName = name;
+}
+
+void Shares::SetSharePrice(Shares* share, float price) {  //set
+	share->sharePrice = price;
+}
+
+void Shares::PrintShare(Shares* share) {  //print
+	cout << "Share Name......:" << share->shareName << endl;
+	cout << "Share Symbol....:" << share->shareSysmbol << endl;
+	cout << "Share Id........:" << share->shareId << endl;
+	cout << "Share Price.....:" << share->sharePrice << endl;
+}
