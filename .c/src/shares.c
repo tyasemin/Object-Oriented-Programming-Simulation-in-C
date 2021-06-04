4b498ba674995b9364be3f7044838332d72a596d
tyasemin <70871014+tyasemin@users.noreply.github.com>
2021-06-04 03:32:49 +0300

    update

--- .c/src/shares.c
+++ .c/src/shares.c
@@ -4,61 +4,66 @@
 Shares Share() {
 	Shares this;
 	this = (Shares)malloc(sizeof(struct SHARES));
-	
-	this->shareId = &set_id; 
-	this->shareName = &set_name;
-	this->sharePrice = &set_price;
-	this->shareSymbol = &set_symbol;
+	this->shareId = &setId;
+	this->shareName = &setName;
+	this->sharePrice = &setPrice;
+	this->shareSymbol = &setSymbol;
+	this->next = NULL;
 
-	this->GetShareId = &get_id;  //Assigning functions to access from within the struct
-	this->GetShareName = &get_name;
-	this->GetSharePrice = &get_price;
-	this->GetShareSymbol = &get_symbol;
+	this->GetShareId = &getId;  //link
+	this->GetShareName = &getName;
+	this->GetSharePrice = &getPrice;
+	this->GetShareSymbol = &getSymbol;
 
-	this->SetShareId = &set_id; 
-	this->SetShareName = &set_name;
-	this->SetSharePrice = &set_price;
-	this->SetShareSymbol = &set_symbol;
+	this->SetShareId = &setId; //link
+	this->SetShareName = &setName;
+	this->SetSharePrice = &setPrice;
+	this->SetShareSymbol = &setSymbol;
 
-	this->PrintShare = &print_share;
-
+	this->PrintShare = &printShare;
+	this->DeleteShare = &deleteShare;
 	return this;
 }
 
-char* get_id(Shares this) { //get
+char* getId(Shares this) { //get
 	return this->shareId;
 }
 
-char* get_name(Shares this) { //get
+char* getName(Shares this) { //get
 	return this->shareName;
 }
 
-float* get_price(Shares this) { //get
+float* getPrice(Shares this) { //get
 	return this->sharePrice;
 }
 
-char* get_symbol(Shares this) { //get
+char* getSymbol(Shares this) { //get
 	return this->shareSymbol;
 }
 
 
-void set_id(Shares this, char* id) { //set
+void setId(Shares this, char* id) { //set
 	this->shareId = id;
 }
-void set_name(Shares this, char* name) { //set
+void setName(Shares this, char* name) { //set
 	this->shareName = name;
 }
-void set_symbol(Shares this, char* symbol) { //set
+void setSymbol(Shares this, char* symbol) { //set
 	this->shareSymbol = symbol;
 }
-void set_price(Shares this, float *price) { //set
+void setPrice(Shares this, float *price) { //set
 	this->sharePrice = price;
 }
 
-void print_share(Shares this) { //print
+void printShare(Shares this) {
 	printf("Share name.....:%s\n", this->shareName);
 	printf("Share symbol..:%s\n", this->shareSymbol);
 	printf("Share id...:%s\n", this->shareId);
 	printf("Share price:%.2f\n", *this->sharePrice);
 
 }
+
+void deleteShare(Shares this) {
+	free(this);
+}
+
